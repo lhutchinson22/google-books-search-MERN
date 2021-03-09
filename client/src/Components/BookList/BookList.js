0d9b08296f8e45edfcd8bb46cc8c2c82d6e3bc0c
Lauren Hutchinson <laurenhutchinson@Laurens-MacBook-Pro.local>
2021-03-09 10:26:48 -0800

    search working and thumbnail entered into a data table

--- client/src/Components/BookList/BookList.js
+++ client/src/Components/BookList/BookList.js
@@ -1,16 +1,25 @@
 import React from "react";
+import "./style.css";
 
-const BookList = ({ result }) => {
+const BookList = ({ result, index }) => {
   return (
-    <div>
-      {result.map((name, index) => (
-        <img
-          key={index}
-          src={name.volumeInfo.imageLinks.thumbnail}
-          alt={name.title}
-        />
-      ))}
-    </div>
+    <table className="table table-dark table-striped">
+      <tbody>
+        {result.map((name, index) => (
+          <tr className="table-active" key={index}>
+            <td>
+              <a target="blank" href={name.volumeInfo.previewLink}>
+                <img
+                  key={index}
+                  src={name.volumeInfo.imageLinks.thumbnail}
+                  alt={name.title}
+                />
+              </a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
