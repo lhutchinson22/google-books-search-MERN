4e9d5048e504492bce255c91958df6424070f1f8
Lauren Hutchinson <laurenhutchinson@Laurens-MacBook-Pro.local>
2021-03-09 16:01:44 -0800

    searching is done, failed post request for saving books

--- client/src/Components/BookList/BookList.js
+++ client/src/Components/BookList/BookList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import SaveButton from "../SaveButton/SaveButton";
 import "./style.css";
+import API from "../../utils/API";
 
 const BookList = ({ result, index }) => {
+  const handleClick = () => {
+    console.log("clicked save");
+
+    API.saveBook()
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <table className="table table-dark table-striped">
+    <table className="table table-hover" style={{ marginTop: "20px" }}>
       <tbody>
         {result.map((name, index) => (
           <tr className="table-active" key={index}>
@@ -15,6 +25,16 @@ const BookList = ({ result, index }) => {
                   alt={name.title}
                 />
               </a>
+              <SaveButton onClick={handleClick} />
+            </td>
+            <td style={{}}>
+              <strong>Book title:</strong> {name.volumeInfo.title}
+              <p style={{}}>
+                <strong>Description:</strong> {name.volumeInfo.description}
+              </p>
+            </td>
+            <td style={{}}>
+              <strong>Authors:</strong> {name.volumeInfo.authors}
             </td>
           </tr>
         ))}
--- client/src/Pages/Saved.js
+++ client/src/Pages/Saved.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import API from "../utils/API";
 
 const Saved = () => {
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  // Loads all books and sets them to books
+  function loadBooks() {
+    API.getBooks()
+      .then((res) => res.data)
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div>
       <h1>This is saved</h1>
+      {loadBooks && loadBooks}
     </div>
   );
 };
